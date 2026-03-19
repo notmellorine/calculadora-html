918066916c544dc4ec2695a19e4da8eed5d48b2b
Aryadne <arya.kusse@gmail.com>
2026-03-19 18:37:17 -0300

    Adicionado as funcionalidades dos botões de apagar.

--- dist/script.js
+++ dist/script.js
@@ -1,19 +1,43 @@
 const buttons = document.querySelectorAll('button');
-const visorElement = document.getElementById('visor');
+let visorElement = document.getElementById('visor');
 buttons.forEach(button => {
     button.addEventListener('click', (event) => {
         const ClickedButton = event.currentTarget;
-        const buttonText = ClickedButton.textContent || ClickedButton.innerText;
-        exib(buttonText);
+        if (ClickedButton.id === 'btn-limpar') {
+            limpar();
+        }
+        else if (ClickedButton.id === 'btn-apagar') {
+            apagar();
+        }
+        else {
+            const buttonText = (ClickedButton.textContent || ClickedButton.innerText).trim();
+            exib(buttonText);
+        }
     });
 });
 function exib(text) {
     if (visorElement) {
-        if (visorElement.innerText === '0' && text !== '.') {
+        if (visorElement.innerText === '0') {
             visorElement.innerText = text;
         }
         else {
             visorElement.innerText += text;
+        }
+    }
+}
+function limpar() {
+    if (visorElement) {
+        visorElement.innerText = '0';
+    }
+}
+function apagar() {
+    if (visorElement && visorElement.innerText !== '0') {
+        let conteudo = visorElement.innerText;
+        if (conteudo.length === 1) {
+            visorElement.innerText = '0';
+        }
+        else {
+            visorElement.innerText = conteudo.substring(0, conteudo.length - 1);
         }
     }
 }
--- src/script.js
+++ src/script.js
@@ -3,17 +3,41 @@ var visorElement = document.getElementById('visor');
 buttons.forEach(function (button) {
     button.addEventListener('click', function (event) {
         var ClickedButton = event.currentTarget;
-        var buttonText = ClickedButton.textContent || ClickedButton.innerText;
-        exib(buttonText);
+        if (ClickedButton.id === 'btn-limpar') {
+            limpar();
+        }
+        else if (ClickedButton.id === 'btn-apagar') {
+            apagar();
+        }
+        else {
+            var buttonText = (ClickedButton.textContent || ClickedButton.innerText).trim();
+            exib(buttonText);
+        }
     });
 });
 function exib(text) {
     if (visorElement) {
-        if (visorElement.innerText === '0' && text !== '.') {
+        if (visorElement.innerText === '0') {
             visorElement.innerText = text;
         }
         else {
             visorElement.innerText += text;
+        }
+    }
+}
+function limpar() {
+    if (visorElement) {
+        visorElement.innerText = '0';
+    }
+}
+function apagar() {
+    if (visorElement && visorElement.innerText !== '0') {
+        var conteudo = visorElement.innerText;
+        if (conteudo.length === 1) {
+            visorElement.innerText = '0';
+        }
+        else {
+            visorElement.innerText = conteudo.substring(0, conteudo.length - 1);
         }
     }
 }
